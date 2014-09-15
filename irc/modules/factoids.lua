372003af0549bf974b609b0e23d802ac54e300b5
Francisco <franciscouzo@gmail.com>
2014-09-15 01:22:25 -0300

    add factoids.lua

---
+++ irc/modules/factoids.lua
@@ -0,0 +1,60 @@
+local utils = require("irc.utils")
+
+return function(irc)
+	local factoids = {}
+	local file = io.open("data/factoids")
+	if file then
+		factoids = utils.unpickle(file:read("*a"))
+		file:close()
+	end
+
+	irc:add_hook("factoid", "on_quit", function(irc)
+		local file = io.open("data/factoids", "w")
+		if file then
+			file:write(utils.pickle(factoids))
+			file:close()
+		end
+	end)
+
+	irc:add_hook("factoid", "on_cmd_privmsg", function(irc, state, channel, msg)
+		msg_no_prefix = msg:match("^%s*" .. utils.escape_pattern(irc.nick) .. "[:,]%s*(.+)%s*")
+
+		local reply = not not msg:find("<reply>")
+		local action = not not msg:find("<action>")
+
+		if reply ~= action then
+			if not msg_no_prefix then
+				return
+			end
+			
+			local trigger, response = unpack(utils.split(msg_no_prefix, reply and "<reply>" or "<action>"))
+			trigger = utils.strip(trigger:gsub("[^%w]", ""):lower())
+			response = utils.strip(response)
+
+			if #response > 300 then
+				irc:notice(state.nick, "reply is too long")
+				return
+			end
+
+			if not factoids[trigger] then
+				factoids[trigger] = {}
+			end
+
+			for _, factoid in ipairs(factoids[trigger]) do
+				if factoid == response then
+					irc:notice(state.nick, "I already know that")
+					return
+				end
+			end
+
+			table.insert(factoids[trigger], response)
+			irc:notice(state.nick, "ok " .. state.nick)
+		else
+			msg = msg_no_prefix or msg
+			local trigger = utils.strip(msg:gsub("[^%w]", ""):lower())
+			if factoids[trigger] and #trigger * (1 / 30) > math.random() then
+				irc:privmsg(channel, utils.random_choice(factoids[trigger]))
+			end
+		end
+	end)
+end
