51c76e72ef3245ddc8642035c220c2247be2e939
Francisco <franciscouzo@gmail.com>
2014-09-17 11:40:02 -0300

    convert matched string to number

--- irc/modules/n_s_n+2_s.lua
+++ irc/modules/n_s_n+2_s.lua
@@ -1,6 +1,8 @@
 return function(irc)
 	irc:add_hook("n_s_n+2_s", "on_cmd_privmsg", function(irc, state, channel, msg)
 		local n1, s1, n2, s2 = msg:match("(%d+)(.+)(%d+)(.+)")
+		n1 = tonumber(n1)
+		n2 = tonumber(n2)
 		if n1 and n1 + 2 == n2 then
 			irc:privmsg(channel, (n1 + 1) .. s1 .. (n2 + 1) .. s2)
 		end
