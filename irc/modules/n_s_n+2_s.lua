e5e93bedd357a9b46321a37837c88dd486008def
Francisco <franciscouzo@gmail.com>
2014-09-17 11:24:46 -0300

    added n_s_n+2_s module

---
+++ irc/modules/n_s_n+2_s.lua
@@ -0,0 +1,8 @@
+return function()
+	irc:add_hook("n_s_n+2_s", "privmsg", function(irc, state, channel, msg)
+		local n1, s1, n2, s2 = msg:match("(%d+)(.+)(%d+)(.+)")
+		if n1 and n1 + 2 == n2 then
+			irc:privmsg(channel, (n1 + 1) .. s1 .. (n2 + 1) .. s2)
+		end
+	end)
+end
