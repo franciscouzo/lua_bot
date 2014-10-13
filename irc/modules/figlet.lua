fe7fe0758da75e28cc2a138fc6fee9a6d556ac49
franciscouzo <franciscouzo@gmail.com>
2014-10-13 11:00:15 -0300

    change space to space byte

--- irc/modules/figlet.lua
+++ irc/modules/figlet.lua
@@ -121,7 +121,7 @@ local function addchar(byte, font, output, kern, smush)
 	end
 
 	for i = 1, font.height do
-		if smush and output[i] ~= "" and byte ~= " " then
+		if smush and output[i] ~= "" and byte ~= (" "):byte() then
 			local lhc = output[i]:sub(-1)
 			local rhc = c[i]:sub(1, 1)
 			output[i] = output[i]:sub(1, -2)
@@ -134,7 +134,7 @@ local function addchar(byte, font, output, kern, smush)
 		else
 			output[i] = output[i] .. c[i]
 		end
-		if not (kern or smush) or byte == " " then
+		if not (kern or smush) or byte == (" "):byte() then
 			output[i] = output[i] .. " "
 		end
 	end
