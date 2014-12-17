1af5afffcd4202deef5ff09a96f880e1d163757c
Francisco <franciscouzo@gmail.com>
2014-12-17 19:30:51 -0300

    save karma after every modification

--- irc/modules/karma.lua
+++ irc/modules/karma.lua
@@ -8,13 +8,12 @@ return function(irc)
 		file:close()
 	end
 
-	irc:add_hook("karma", "on_quit", function(irc)
-		local file = io.open("data/karma", "w")
-		if file then
-			file:write(utils.pickle(karma))
-			file:close()
-		end
-	end)
+	local function save_karma()
+		os.rename("data/karma", "data/karma.backup")
+		local file = assert(io.open("data/karma", "w"))
+		file:write(utils.pickle(karma))
+		file:close()
+	end
 	
 	irc:add_hook("karma", "on_cmd_privmsg", function(irc, state, channel, msg)
 		local nick, plus_or_minus = msg:sub(1, -3), msg:sub(-2)
@@ -24,8 +23,10 @@ return function(irc)
 		end
 		if plus_or_minus == "--" then
 			karma[nick] = (karma[nick] or 0) - 1
+			save_karma()
 		elseif plus_or_minus == "++" then
 			karma[nick] = (karma[nick] or 0) + 1
+			save_karma()
 		end
 	end)
 	irc:add_command("karma", "karma", function(irc, state, channel, msg)
