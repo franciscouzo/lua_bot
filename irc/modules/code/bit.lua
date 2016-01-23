efda424a2e4d9beba5e6e4e60421c71792463f26
Francisco <franciscouzo@gmail.com>
2016-01-23 14:19:53 -0300

    Use updated bit32 library

--- irc/modules/code/bit.lua
+++ irc/modules/code/bit.lua
@@ -1,5 +1,5 @@
 return function(irc)
-	require("bit32")
+	local bit32 = require("bit32")
 	for func_name, func in pairs(bit32) do
 		local command = (func_name:sub(1, 1) == "b") and func_name:sub(2, -1) or func_name
 		irc:add_command("bit", "bit." .. command, function(irc, state, channel, ...)
