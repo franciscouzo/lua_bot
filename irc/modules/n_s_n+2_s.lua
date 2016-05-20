3cc8f18012adb88c0b44ebdd5ea445e4630c4044
Francisco <franciscouzo@gmail.com>
2016-05-19 21:55:14 -0300

    Add some constraints to the n_s_n+2_s module

--- irc/modules/n_s_n+2_s.lua
+++ irc/modules/n_s_n+2_s.lua
@@ -1,9 +1,10 @@
 return function(irc)
 	irc:add_hook("n_s_n+2_s", "on_cmd_privmsg", function(irc, state, channel, msg)
-		local n1, s1, n2, s2 = msg:match("(%d+)(%D+)(%d+)(%D+)")
+		local n1, s1, n2, s2 = msg:match("^(%d+)(%l+)(%d+)(%l+)$")
 		n1 = tonumber(n1)
 		n2 = tonumber(n2)
-		if n1 and n1 + 2 == n2 then
+		if n1 and n1 + 2 == n2 and n2 < 20 and
+		   #s1 <= 10 and #s2 <= 10 then
 			irc:privmsg(channel, (n1 + 1) .. s1 .. (n2 + 1) .. s2)
 		end
 	end)
