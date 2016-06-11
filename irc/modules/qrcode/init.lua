271daa60704bc80a87a84f8f5fae99d1021c1e13
Francisco Couzo <franciscouzo@gmail.com>
2016-06-11 19:30:55 -0300

    Use unicode characters instead of irc colors

--- irc/modules/qrcode/init.lua
+++ irc/modules/qrcode/init.lua
@@ -1,6 +1,7 @@
 local qrcode = require("irc.modules.qrcode.qrencode").qrcode
 
 return function(irc)
+	local blocks = {" ", "▀", "▄", "█"}
 	irc:add_command("qrcode", "qrcode_image", function(irc, state, channel, msg)
 		local matrix = select(2, assert(qrcode(msg)))
 		local padding = 2
@@ -10,8 +11,7 @@ return function(irc)
 			for x = 1, #matrix + padding * 2 do
 				local foreground = ((matrix[x - padding] or {})[y - padding + 1] or -1) >= 0 and 1 or 0
 				local background = ((matrix[x - padding] or {})[y - padding]     or -1) >= 0 and 1 or 0
-
-				table.insert(line, ("\003%i,%i▄"):format(foreground, background))
+				table.insert(line, blocks[foreground * 2 + background + 1])
 			end
 			irc:privmsg(channel, table.concat(line))
 		end
